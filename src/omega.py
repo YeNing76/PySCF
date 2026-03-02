b452d048ba2d12c76f76608cfe3bf4f5767a199c
YeNing76 <z349j@unb.ca>
2026-03-02 10:54:56 -0400

    adding import package

--- src/omega.py
+++ src/omega.py
@@ -2,6 +2,9 @@
 # omega_matr(vor, points, neighbors) so that the FWHT
 # can be used to improve the effeciency
 # 2026, 01, 24
+import math
+import numpy as np
+
 def omega_matr_fwht(vor, points, neighbors):
     length = len(points)
     num_bits = math.ceil(math.log2(length))
@@ -35,3 +38,39 @@ def omega_matr_fwht(vor, points, neighbors):
             omega[m][n] = a[n] / dim
 
     return omega
+
+# The following code is a new version for the eqn. (21) 
+# 2026-02-20
+def t_bar_matr(points, neighbors, sigma, volumes):
+    # Laplacian (already symmetric)
+    L_sym = laplacian(points, neighbors, sigma, volumes)
+
+    # electron–nucleus attraction
+    ele_nu, _ = coulombic_potential(points)
+
+    length = len(points)
+    Tbar = np.zeros((length, length))
+
+    for m in range(length):
+        for n in range(length):
+            Tbar[m, n] = -0.5 * L_sym[m, n]
+
+        # add nuclear attraction to diagonal
+        Tbar[m, m] += -ele_nu[m] 
+
+    return Tbar   
+
+
+"""In-place Fast Walsh–Hadamard Transform on last axis."""
+def fwht(a):
+    h = 1
+    n = len(a)
+    while h < n:
+        step = h * 2
+        for i in range(0, n, step):
+            for j in range(i, i + h):
+                x = a[j]
+                y = a[j + h]
+                a[j] = x + y
+                a[j + h] = x - y
+        h *= 2

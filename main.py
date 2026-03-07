9844bb5cb72c321607afbc2e56950f20d496c4e0
YeNing76 <z349j@unb.ca>
2026-03-06 20:43:24 -0400

    Modifying input of t_bar_mar

--- main.py
+++ main.py
@@ -15,5 +15,23 @@ def main():
     vals, vecs = np.linalg.eigh(H)
     print(vals[0])
 
+    # test pauli_lcu_one_body_from_omega and t_bar
+    eta = 1 # for single electron system
+    omega_pg = om.omega_matr_fwht(vor, points, neighbors, sigma, volumes)
+    omega_pg = np.array(omega_pg)
+    H_pauli = pl.pauli_lcu_one_body_from_omega(omega_pg, eta)
+    H_from_pauli = H_pauli.to_matrix()
+
+    num_qubits = 10
+    dim = 2**num_qubits
+    length = len(points)
+    t_bar_small = om.t_bar_matr(points, neighbors, sigma, volumes)
+    t_bar = [[0.0]*dim for _ in range(dim)]
+    for i in range(length):
+        for j in range(length):
+            t_bar[i][j] = t_bar_small[i][j]
+    err = np.linalg.norm(H_from_pauli - t_bar)
+    print("err =", err)
+
 if __name__=="__main__":
     main()
